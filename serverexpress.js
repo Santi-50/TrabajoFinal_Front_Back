5f3c6b97b5db2d84ef3e82fec5061f1e0a97e760
Santiago <santiagoshaila@hotmail.com>
2025-06-04 23:52:49 -0300

    api calendario

--- serverexpress.js
+++ serverexpress.js
@@ -3,12 +3,28 @@ const app = express()
 const HOSTNAME = '127.0.0.1';
 const PORT = 3000;
 
+const fs = require('node:fs')
+const {Apicalendario} = require ('./src/ApiProyecto')
+
+
+const HOME = fs.readFileSync('./index.html')
+//const GRUPO = fs.readFileSync('./grupo.html')
+
+
 app.get('/', function (req, res) {
     res.send('<h1>HOLA MUNDO </h1>')
 })
 
+
 app.get('/grupo', function (req, res) {
     res.send('<h1>HOLA grupo de trabajo final</h1>')
+})
+
+app.get('/api/calendario', (req, res) => {
+    res.setHeader('Content-type', 'application/json')
+    res.status(200)
+    res.send(JSON.stringify(Apicalendario))
+    //res.send(JSON.stringify(Apicalendario))
 })
 
 app.listen (PORT, HOSTNAME, () =>{
---
+++ src/ApiProyecto.js
@@ -0,0 +1,315 @@
+let Apicalendario = {
+Julio: [
+    {
+        dia: 1,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 2,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 3,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 4,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 5,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 6,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 7,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 8,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 9,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 10,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 11,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 12,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 13,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 14,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 15,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 16,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 17,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 18,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 19,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 20,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 21,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 22,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 23,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 24,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 25,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 26,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 27,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 28,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 29,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 30,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 31,
+        horario: "00:00",
+        actividad: "",
+    },
+    ],
+    
+Agosto: [
+    {
+        dia: 1,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 2,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 3,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 4,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 5,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 6,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 7,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 8,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 9,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 10,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 11,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 12,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 13,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 14,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 15,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 16,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 17,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 18,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 19,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 20,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 21,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 22,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 23,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 24,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 25,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 26,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 27,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 28,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 29,
+        horario: "00:00",
+        actividad: "",
+    },
+    {
+        dia: 31,
+        horario: "00:00",
+        actividad: "",
+    },
+
+]
+}
+
+module.exports.Apicalendario = Apicalendario;
