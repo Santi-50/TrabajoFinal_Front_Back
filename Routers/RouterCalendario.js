c83005ae52a6be74012655a7b87c07643c9a0c3f
Santiago <santiagoshaila@hotmail.com>
2025-06-17 19:38:32 -0300

    Guardando actualizacion agosto en Rama_Santiago

---
+++ Routers/RouterCalendario.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const routerCalendario = express.Router();
+
+routerCalendario.use(express.json());
+
+const Calendariocopntroller = require('../controller/calendarioController')
+
+routerCalendario.get('/', Calendariocopntroller.readcalendario)
+routerCalendario.get('/julio', Calendariocopntroller.readcalendariojulio)
+routerCalendario.get('/agosto', Calendariocopntroller.readcalendarioAgosto)
+routerCalendario.post('/agosto', Calendariocopntroller.createActividadagosto)
+routerCalendario.post('/julio', Calendariocopntroller.createActividadjulio)
+routerCalendario.patch('/julio/:dia', Calendariocopntroller.updateActividadJulio)
+routerCalendario.patch('/agosto/:dia', Calendariocopntroller.updateActividadAgosto)
+
+
+
+module.exports = routerCalendario;
